6d5d5a31ebd148b70568302ddf24e39dc80abe8f
Sri Satya <srisatya704@gmail.com>
2023-05-04 14:00:06 -0500

    Commit

--- GitFirstProject/src/CommitAndPush.java
+++ GitFirstProject/src/CommitAndPush.java
@@ -3,7 +3,7 @@ public class CommitAndPush {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Commit and Push");
+		System.out.println("First Commit and Push");
 
 	}
 
